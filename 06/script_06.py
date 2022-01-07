440ea96e3d318952fc550091c87630b0a660ec38
Maksim Vasilev <sukharevsky@outlook.com>
2022-01-07 18:19:46 +0300

    Fixed 2-nd task

--- 06/script_06.py
+++ 06/script_06.py
@@ -1,3 +1,5 @@
+import re
+
 logs = [
     'May 18 11:59:18 PC-00102 plasmashell[1312]: kf.plasma.core: findInCache with a lastModified timestamp of 0 is deprecated',
     'May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations.',
@@ -12,22 +14,14 @@ logs = [
     'May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users.'
 ]
 
-log = []
-
-for rec in logs:
-    d = {} 
-    log_split = rec.split(' ')
-    d['date'] = ' '.join(log_split[:2])
-    d['time'] = ' '.join(log_split[2])
-    d['pc_name'] = log_split[3]
-    d['service_name'] = log_split[4].replace(":", "")
-    d['message'] = ' '.join(log_split[5:])
-    log.append(d)
-
 # Загрузите в файл с именем 'file_6.txt' строки этого списка за 20 мая
 
-for rec in log:
-    if rec['date'] == 'May 20':
+regexp = re.compile('^(\\bMay 20\\b)[^$]')
+print(regexp)
+
+for rec in logs:
+    print(regexp.search(rec))
+    if regexp.search(rec) != None:
         with open('file_6.txt', 'a+', encoding='utf-8') as file_1:
             file_1.write(str(rec))
             file_1.write('\n')
@@ -35,8 +29,8 @@ for rec in log:
 
 # Считайте из этого файла время первой записи. Ничего кроме времени считывать не нужно! Выведите это время на экран.
 
-with open('file_6.txt', 'r+', encoding='utf-8') as file_1:
-    # content = file_1.read()
-    print(file_1.readline())
-    file_1.close()
+# with open('file_6.txt', 'r+', encoding='utf-8') as file_1:
+#     # content = file_1.read()
+#     print(file_1.readline())
+#     file_1.close()
 
